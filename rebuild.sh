85ed331be35a2950f1f790ced52c8fbdec503463
Vladislav <zamuhrishnik@yandex.ru>
2026-03-08 13:53:24 +0000

    Fix: Stop tracking infra files

--- rebuild.sh
+++
@@ -1,7 +0,0 @@
-docker build -t boar-game-dev . && docker rm -f boar-game-dev && docker run -d \
-  --name boar-game-dev \
-  -p 3001:3000 \
-  -v /projects/boars-dev/database.db:/app/database.db \
-  -e APP_ROOT=/boar-game-dev \
-  --restart unless-stopped \
-  boar-game-dev
